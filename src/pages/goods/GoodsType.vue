56435d211fe8d25ff7780cff79efa16a755ad804
lk <354378485@qq.com>
2023-12-27 23:15:03 +0800

    feat: 商品分类页搭建

---
+++ src/pages/goods/GoodsType.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+    
+</script>
+<template>
+    <view class="goods-type-container">
+        <view class="goods-type-left"></view>
+        <view class="goods-type-right"></view>
+    </view>
+</template>
+<style scoped lang="scss">
+.goods-type-container{
+    background-color: red;
+    display: flex;
+    flex-wrap: wrap;
+}
+.goods-type-left{
+    flex: auto;
+}
+.goods-type-right{
+    flex: auto;
+}
+
+</style>
