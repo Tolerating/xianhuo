0625cb56fd53cc08f1cee97d5a287bd6ca844019
lk <354378485@qq.com>
2024-01-04 21:08:08 +0800

    style: 聊天界面搭建

--- src/pages/home/message.vue
+++ src/pages/home/message.vue
@@ -1,13 +1,58 @@
 <template>
-	<view class="">
-		消息页面
-	</view>
+	<WaterFullLayout :statusBar="true">
+		<template #navigateBar>
+			<view class="navigate-bar" >
+				<view>
+					<text class="navigate-bar-title">消息</text>
+					<view class="xh-btn-group">
+						<uni-icons
+							type="trash-filled"
+							color="gray"
+							size="16"
+						/>
+						清除未读
+					</view>
+				</view>
+				<uni-icons
+					type="gear-filled"
+					color="gray"
+					size="24"
+				/>
+			</view>
+			<view class="top">
+				<view class="cd"></view>
+			</view>
+		</template>
+	</WaterFullLayout>
 </template>
 
 <script lang="ts" setup>
-
+import WaterFullLayout from '@/components/WaterFullLayout.vue'
 </script>
 
 <style lang="scss" scoped>
+.navigate-bar{
+	background-color: white;
+	display: flex;
+	padding: 0 $xianhuo-padding-LR;
+    .navigate-bar-title {
+		font-size: 24px;
+		font-weight: bold;
+    }
+    .xh-btn-group {
+		background-color: $xh-theme-color-50;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		border-radius: 10px;
+		font-size: 16px;
+		color: $xh-text-color-C;
+    }
 
+}
+.top {
+    .cd {
+    }
+}
 </style>
