56c39653b8a9fad2f19cbbac08e7e68019cf3e39
lk <354378485@qq.com>
2024-01-11 22:20:53 +0800

    style: 完善界面

--- src/pages/home/release/releaseGoods.vue
+++ src/pages/home/release/releaseGoods.vue
@@ -1,13 +1,20 @@
 <!-- 商品发布页 -->
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { reactive } from 'vue';
 import { ref } from 'vue';
+import { allCategories } from '@/api/home/goods'
+import type { Category } from '@/types/Category';
+import useUserStore from '@/stores/users';
+import { watch } from 'vue';
+import { nextTick } from 'vue';
 
 const StatusBarHeight = uni.getSystemInfoSync().statusBarHeight
 const statusBarHeight = ref<number>(Number(StatusBarHeight))
 const goodsPrice = ref<number>()
 const selectImgs = reactive<Map<string, object>>(new Map())
 const releaseForm = reactive<{
+    category: number,
     // 现价
     currentPrice: string,
     // 原价
@@ -17,8 +24,10 @@ const releaseForm = reactive<{
     // 商品图片
     imageList: any[],
     // 商品描述
-    description: string
+    description: string,
+    [key:string]:unknown
 }>({
+    category: 1,
     currentPrice: "",
     originPrice: "",
     bargainDown: [{ value: 0, text: "不砍价" }],
@@ -26,57 +35,80 @@ const releaseForm = reactive<{
     description: ""
 })
 
+
+
+// 存放商品分类
+const categoryList = reactive<Category[]>([])
 // 焦点是否在现价输入框内，一开始为null
 const isCurrentPrice = ref<boolean | null>(null)
-
+const categorySelected = ref<number>(0)
 // 价钱弹出层引用
 const pricePopup = ref()
 // 发货方式弹出层引用
-const dispatchPopup = ref()
+const sellPopup = ref()
 // 商品类别右侧抽屉
 const goodsTypeRight = ref()
 const showuPricePopup = () => {
     pricePopup.value.open()
 }
-const showDispatchWay = () => {
-    dispatchPopup.value.open()
+const showSellhWay = () => {
+    sellPopup.value.open()
 }
-// setInterval(()=>{
-//     const selction = getSelection()
-//     console.log(selction)
 
-// }, 1000)
-// 接受键盘组件的input事件，根据isCurrentPrice分别为currentPrice和originPrice赋值
-const setPrice = (value: string) => {
-    console.log(isCurrentPrice.value, value);
+// 点击价格输入弹出层弹窗触发
+const savePrice = () => {
 
-    if (isCurrentPrice.value == null) return
-    if (isCurrentPrice.value) {
-        releaseForm.currentPrice += value
-    } else {
-        releaseForm.originPrice += value
+}
+
+const dealFloatNumber = (value:string,property:string)=>{
+    if (value.indexOf('-') != -1) {
+        nextTick(() => {
+            releaseForm[property] = ""
+        })
+        return
     }
 
+    if (value.indexOf(".") != -1 && value.split(".")[1].length > 2) {
+        nextTick(() => {
+            releaseForm[property] = value.split(".")[0] + "." + value.split(".")[1].slice(0, 2)
+        })
+    }
 }
-// 隐藏价格输入弹出层
-const hidePricePopup = () => {
-    pricePopup.value.close()
+
+const dealPrice = (e: Event) => {
+    //@ts-ignore
+    const { value } = e.detail
+    if(isCurrentPrice){
+        dealFloatNumber(value,'currentPrice')
+    }else{
+        dealFloatNumber(value,'originPrice')
+    }
+    
 }
+
+
 
 const showImgList = (e: any) => {
     selectImgs.set(e.tempFiles[0].uuid, e.tempFiles[0])
-    console.log(selectImgs);
 
 
 }
 const deleteImg = (e: any) => {
     selectImgs.delete(e.tempFile.uuid)
-    console.log(selectImgs);
 
 }
-const showTypeRight= ()=>{
+const showTypeRight = () => {
     goodsTypeRight.value.open()
 }
+const getCategories = async () => {
+    const result = await allCategories();
+    categoryList.push(...result.data)
+
+}
+
+onMounted(() => {
+    getCategories()
+})
 </script>
 <template>
     <view class="release-goods-container">
@@ -106,7 +138,7 @@ const showTypeRight= ()=>{
                 <text class="property-item-left">商品分类</text>
                 <view style="flex: 1;"></view>
                 <view class="property-item-right">
-                    <text>衣服/长袖</text>
+                    <text>{{ categoryList[releaseForm.category - 1]?.name }}</text>
                     <uni-icons type="right" color="gray" size="24" />
                 </view>
             </view>
@@ -115,13 +147,13 @@ const showTypeRight= ()=>{
                 <text class="property-item-left">价格</text>
                 <view style="flex: 1;"></view>
                 <view class="property-item-right">
-                    <text>￥0.00</text>
+                    <text style="color: red;font-weight: bold;">￥{{ Number(releaseForm.currentPrice) }}</text>
                     <uni-icons type="right" color="gray" size="24" />
                 </view>
             </view>
             <hr />
-            <view class="property-item" @tap="showDispatchWay">
-                <text class="property-item-left">发货方式</text>
+            <view class="property-item" @tap="showSellhWay">
+                <text class="property-item-left">售卖模式</text>
                 <view style="flex: 1;"></view>
                 <view class="property-item-right">
                     <text>邮寄</text>
@@ -130,33 +162,55 @@ const showTypeRight= ()=>{
             </view>
         </view>
         <!-- 价钱弹出层 -->
-        <uni-popup ref="pricePopup" type="center">
+        <uni-popup ref="pricePopup" @maskClick="savePrice" type="center">
             <view class="input-container">
                 <view class="price-group">
                     <text>现价</text>
-                    <input placeholder="￥0.00" v-model="releaseForm.currentPrice" @focus="isCurrentPrice = true"
-                        type="digit">
-                    <uni-data-checkbox multiple :localdata="releaseForm.bargainDown"></uni-data-checkbox>
+                    <input placeholder="￥0.00" @input="dealPrice" v-model="releaseForm.currentPrice"
+                        @focus="isCurrentPrice = true" type="digit">
+                    <!-- <uni-data-checkbox multiple :localdata="releaseForm.bargainDown"></uni-data-checkbox> -->
                 </view>
                 <hr>
                 <view class="price-group">
                     <text style="font-size: 14px;">原价</text>
-                    <input style="font-size: 14px;" @focus="isCurrentPrice = false" v-model="releaseForm.originPrice"
+                    <input style="font-size: 14px;" @input="dealPrice" @focus="isCurrentPrice = false" v-model="releaseForm.originPrice"
                         placeholder="￥0.00" type="digit">
                 </view>
             </view>
         </uni-popup>
-        <!-- 发货方式弹出层 -->
-        <uni-popup ref="dispatchPopup" type="bottom">
-
+        <!-- 售卖方式弹出层 -->
+        <uni-popup ref="sellPopup" type="bottom">
+            <view class="sell-container">
+                <view class="sell-item"></view>
+                <uni-section type="line" class="sell-item" title="售卖模式">
+                    <uni-data-checkbox
+                        style="padding-left: 10px;"
+                        :localdata="[{value:0,text:'物品出售'},{value:1,text:'物品出租'},{value:2,text:'物品交换'}]"
+                        mode="button"
+                    />
+                </uni-section>
+                <uni-section type="line" class="sell-item" title="发货方式">
+                    <text style="padding-left: 10px;">自行协商·自提 | 送货上门 | 约定交易地点 | 当面验货交易</text>
+                </uni-section>
+                <uni-section type="line" class="sell-item" title="商品要求">
+                    <uni-data-checkbox
+                        style="padding-left: 10px;"
+                        :localdata="[{value:0,text:'不砍价'},{value:1,text:'仅限本校'},{value:2,text:'不支持退换货'}]"
+                        multiple
+                        mode="button"
+                    />
+                </uni-section>
+            </view>
         </uni-popup>
-        <!-- 商品类别右侧抽屉 -->
-        <uni-drawer ref="goodsTypeRight" mode="right" :mask-click="false">
-			<scroll-view style="height: 100%;" scroll-y="true">
-				<button  type="button">关闭Drawer</button>
-				<view v-for="item in 60" :key="item">可滚动内容 {{ item }}</view>
-			</scroll-view>
-		</uni-drawer>
+        <!-- 商品类别弹出层 -->
+        <uni-popup ref="goodsTypeRight" type="bottom">
+            <view class="goods-category-container">
+                <view :class="{ 'category-selected': item.id == categorySelected }" class="category-item "
+                    v-for="item in categoryList" @tap="categorySelected = item.id" :key="item.id">
+                    {{ item.name }}
+                </view>
+            </view>
+        </uni-popup>
     </view>
 </template>
 <style scoped lang="scss">
@@ -256,8 +310,7 @@ $bg-color: rgba(231, 231, 231, .6);
 
     .input-container {
         background-color: white;
-        padding: 10px;
-
+        padding: 20px;
         .price-group {
             display: flex;
             align-items: center;
@@ -278,10 +331,38 @@ $bg-color: rgba(231, 231, 231, .6);
             opacity: .3;
         }
 
-        .price-group {
-            input {}
+    }
+
+    .goods-category-container {
+        background-color: white;
+        display: flex;
+        flex-wrap: wrap;
+        row-gap: 10px;
+        padding: 30px 10px 150px 10px;
+        column-gap: 5px;
+
+        .category-item {
+            background-color: rgba($color: $xh-color-info, $alpha: .4);
+            border-radius: 15px;
+            padding: 5px;
+            text-align: center;
+            flex: 0 0 calc(90%/3)
         }
 
+        .category-item.category-selected {
+            background-color: rgba($color: $xh-color-primary, $alpha: .6);
+            color: $xh-text-color-highlight;
+        }
+    }
+
+    .sell-container{
+        background-color: white;
+        padding-bottom: 100px;
+        // display: flex;
+
+        .sell-item{
+            margin-bottom: 15px;
+        }
     }
 }
 </style>
