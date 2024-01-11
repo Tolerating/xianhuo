75e5a79d0965ba260f46baab27b816d5ad210ae0
lk <354378485@qq.com>
2024-01-11 14:27:02 +0800

    style：添加登录api

--- src/api/user/login.ts
+++ src/api/user/login.ts
@@ -1,10 +1,11 @@
 import request from '@/plugin/request'
+import type { LoginUser } from '@/types/Users';
 import type { ResponseResult } from '@/types/common';
 
 
 
 //登录
-const login = (data):Promise<ResponseResult>=>request("/user/login",data,"POST");
+const login = (data:LoginUser)=>request<string>("/user/login",data,"POST");
 
 // 注册API
 /* data结构
@@ -15,7 +16,7 @@ const login = (data):Promise<ResponseResult>=>request("/user/login",data,"POST")
 	code:""
 }
 */
-const register = (data)=>request('/user/register',data,"POST");
+// const register = (data)=>request('/user/register',data,"POST");
 
 // 修改密码
 /*data结构
@@ -25,5 +26,5 @@ const register = (data)=>request('/user/register',data,"POST");
 	code:""
 }
 */
-const changePassword = (data)=>request('/user/modifyPassword/phone',data,"POST");
-export {login,register,changePassword}
+// const changePassword = (data)=>request('/user/modifyPassword/phone',data,"POST");
+export {login}
