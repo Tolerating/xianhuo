8e0b1cae77e6a6ccd8bc91945da60a5e62793fbe
lk <354378485@qq.com>
2024-01-14 22:47:11 +0800

    feat: 新增售卖模式、发货方式、商品要求接口

--- src/api/home/goods.ts
+++ src/api/home/goods.ts
@@ -1,11 +1,25 @@
 import request from '@/plugin/request'
 import type { Category } from '@/types/Category';
-import type { ResponseResult } from '@/types/common';
-
+import type { DispatchMode } from '@/types/DispatchMode';
+import type { ProductRequire } from '@/types/ProductRequire';
+import type { SellMode } from '@/types/SellMode';
+const Authorization = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxIiwiaWF0IjoxNzA0OTQxMjg1LCJzdWIiOiIiLCJpc3MiOiIiLCJleHAiOjE3MDc2MTk2ODV9.ly8_wfQs8yCRhr3806AjhfqIQWywfPMzWWc9eybvnOs"
+// 获得所有分类
 const allCategories = () => request<Category[]>('/categories', {}, "GET"
-    , { Authorization: "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxIiwiaWF0IjoxNzA0OTQxMjg1LCJzdWIiOiIiLCJpc3MiOiIiLCJleHAiOjE3MDc2MTk2ODV9.ly8_wfQs8yCRhr3806AjhfqIQWywfPMzWWc9eybvnOs" })
+    , { Authorization })
 
+// 获得售卖模式
+const allSellMode = ()=>request<SellMode[]>('/sellMode/1',{},"GET",{Authorization})
+
+// 根据售卖模式获取发货方式
+const dispatchModeBySell = (sellModeId:number)=>request<DispatchMode[]>(`/dispatchMode/${sellModeId}`,{},"GET",{Authorization})
+
+// 根据售卖模式id和发货方式id获取商品要求
+const productRequireByDispatch = (sellModeId:number,dispatchModeId:number)=>request<ProductRequire[]>(`/productRequire/${sellModeId}/${dispatchModeId}`,{},"GET",{Authorization})
 
 export {
-    allCategories
+    allCategories,
+    allSellMode,
+    dispatchModeBySell,
+    productRequireByDispatch
 }
