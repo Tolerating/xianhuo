1c948eed63b978fb5a98e6a93ba97c82bf697c7d
lk <354378485@qq.com>
2024-01-19 17:01:11 +0800

    style: 第一次进入APP时获取网络数据

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
-<script lang="ts">
-	export default {
-		// onLaunch: function() {
-		// 	console.log('App Launch')
-		// },
-		// onShow: function() {
-		// 	console.log('App Show')
-		// },
-		// onHide: function() {
-		// 	console.log('App Hide')
-		// }
-	}
+<script lang="ts" setup>
+import useProductStore from '@/stores/product/index'
+import useUserStore from './stores/users';
+import { onLaunch } from '@dcloudio/uni-app';
+const productStore = useProductStore()
+const userStore = useUserStore()
+
+onLaunch(() => {
+	productStore.requestSellMode()
+	productStore.requestCategory()
+	productStore.requestAllDispatchMode()
+	productStore.requestAllProductRequire()
+	userStore.getUserInfo()
+})
+
 </script>
 
 <style>
-	/*每个页面公共css */
-	/* @import url("./assert/css/variable.scss"); */
+/*每个页面公共css */
+/* @import url("./assert/css/variable.scss"); */
 </style>
--- src/stores/users/index.ts
+++ src/stores/users/index.ts
@@ -1,25 +1,35 @@
+import type { User } from '@/types/Users'
 import {defineStore} from 'pinia'
 import {ref,reactive} from 'vue'
+import {requestUserInfo} from '@/api/user/user'
 
 const useUserStore = defineStore("user",()=>{
-	const userInfo = reactive({})
+	const userInfo = reactive<User | {}>({})
 	const authorization = ref<string | null>(null)
+
 	function updateAuthorization(value:string):void{
 		authorization.value = value
 	}
 	function deleteAuthorization(){
 		authorization.value = ''
 	}
+	async function getUserInfo(){
+		const result = await requestUserInfo()
+		Object.assign(userInfo,result.data)
+		return result.data
+
+	}
 	return{
 		userInfo,
 		authorization,
 		deleteAuthorization,
-		updateAuthorization
+		updateAuthorization,
+		getUserInfo
 	}
 },{
 	persist:{
 		key:"xh_user",
-		paths:['authorization'],
+		paths:['authorization',"userInfo"],
 		storage:{
 			setItem(key,value) {
 				console.log("setitem",value)
