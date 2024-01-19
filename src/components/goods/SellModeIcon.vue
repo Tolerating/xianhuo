054940fbb7d9bfdd11533d018fa7156495635639
lk <354378485@qq.com>
2024-01-19 16:56:46 +0800

    style: 封装商品售卖方式组件

---
+++ src/components/goods/SellModeIcon.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+type SellMode = "出售" | "出租" | "交换";
+const props = defineProps<{type:SellMode}>()
+</script>
+<template>
+    <view class="sell-mode-icon">
+        <text>{{ type }}</text>
+    </view>
+</template>
+<style lang="scss" scoped>
+.sell-mode-icon{
+    text{
+        background-color: rgba($color:$xh-theme-color-500, $alpha: .7);
+        font-size: $xh-font-size-lg;
+        padding: 5px;
+        border-radius: 5px;
+    }
+}
+</style>
