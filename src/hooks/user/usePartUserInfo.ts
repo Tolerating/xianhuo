6ea5547887e6b72592a2697681819ce273ef57b1
lk <354378485@qq.com>
2024-01-24 22:26:03 +0800

    style: 获取用户部分信息hook

---
+++ src/hooks/user/usePartUserInfo.ts
@@ -0,0 +1,22 @@
+import type { UserPart } from "@/types/Users";
+import { reactive, readonly } from "vue";
+import {userInfoById} from '@/api/user/user'
+import {APP_BASE_URL} from '@/config/index'
+
+export function usePartUserInfo(){
+    const partUserInfo = reactive<UserPart>({
+        name:"",
+        school:"",
+        avatar:""
+    })
+
+    async function requestPartUserInfo(uId:number){
+        const result = await userInfoById(uId)
+        Object.assign(partUserInfo,result.data)
+        partUserInfo.avatar = APP_BASE_URL + partUserInfo.avatar 
+    }
+    return{
+        partUserInfo,
+        requestPartUserInfo
+    }
+}
