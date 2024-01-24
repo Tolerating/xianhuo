1f666d17af5f197863b8dabcf5d764c761d00ff2
lk <354378485@qq.com>
2024-01-24 22:28:39 +0800

    style: 用户头像增加网络地址前缀

--- src/stores/users/index.ts
+++ src/stores/users/index.ts
@@ -2,9 +2,10 @@ import type { User } from '@/types/Users'
 import {defineStore} from 'pinia'
 import {ref,reactive} from 'vue'
 import {requestUserInfo} from '@/api/user/user'
+import {APP_BASE_URL} from '@/config/index'
 
 const useUserStore = defineStore("user",()=>{
-	const userInfo = reactive<User | {}>({})
+	const userInfo = reactive<User>({} as User)
 	const authorization = ref<string | null>(null)
 
 	function updateAuthorization(value:string):void{
@@ -16,6 +17,7 @@ const useUserStore = defineStore("user",()=>{
 	async function getUserInfo(){
 		const result = await requestUserInfo()
 		Object.assign(userInfo,result.data)
+		userInfo.avatar = APP_BASE_URL + userInfo.avatar
 		return result.data
 
 	}
