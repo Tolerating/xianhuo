53a84372b43bf46b497dc38f69998485629a399d
lk <354378485@qq.com>
2024-01-27 22:36:08 +0800

    style: 新增注册与修改密码接口

--- src/api/user/login.ts
+++ src/api/user/login.ts
@@ -1,5 +1,5 @@
 import { request } from '@/plugin/request'
-import type { LoginUser } from '@/types/Users';
+import type { LoginUser, User } from '@/types/Users';
 
 
 
@@ -10,12 +10,13 @@ const login = (data: LoginUser) => request<string>("/login", data, "POST");
 const getCheckCode = (email: string) => request<string>(`/mailCode?email=${email}`, {}, "GET")
 
 // 注册API
-
+const registerXH = (data:User)=>request<string>("/register",data,"POST")
 
 // 修改密码
-
-// const changePassword = (data)=>request('/user/modifyPassword/phone',data,"POST");
+const updatePassword = (data:User)=>request<string>('/updatePassword',data,"POST");
 export {
 	login,
-	getCheckCode
+	getCheckCode,
+	registerXH,
+	updatePassword
 }
