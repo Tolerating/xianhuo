72213f4d0d7ff5300a1c15fa69483d3d084dcb7b
lk <354378485@qq.com>
2024-01-28 22:57:53 +0800

    style: 封装状态栏组件

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const statusBarHeight = ref<number>(Number(uni.getSystemInfoSync().statusBarHeight));
+</script>
+<template>
+    <view :style="{ height: statusBarHeight + 'px',backgroundColor:'white' }"></view>
+</template>
+<style lang="scss" scoped>
+
+</style>
