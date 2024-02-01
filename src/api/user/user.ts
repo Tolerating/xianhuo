822385f681a189b980202431228682f049295a31
lk <354378485@qq.com>
2024-02-01 21:03:24 +0800

    style：完善个人信息接口

--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -1,5 +1,5 @@
 import { request } from "@/plugin/request";
-import type { User } from "@/types/Users";
+import type { User, UserPart } from "@/types/Users";
 
 // 根据用户id获得用户部分信息，用于商品详情页展示
 const userInfoById = (userId:number)=>request<User>(`/user/${userId}`,{},"GET")
@@ -7,7 +7,11 @@ const userInfoById = (userId:number)=>request<User>(`/user/${userId}`,{},"GET")
 // 根据authorization获取用户信息
 const requestUserInfo = ()=>request<User>("/user",{},"GET")
 
+// 用户首次登录完善个人信息
+const improveInfo = (data:UserPart)=>request("/improveInfo",data,"POST")
+
 export {
     userInfoById,
-    requestUserInfo
+    requestUserInfo,
+    improveInfo
 }
