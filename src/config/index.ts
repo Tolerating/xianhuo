5175518d626ebec8a63f4c2f8a628fda7e616655
lk <354378485@qq.com>
2024-02-01 21:03:05 +0800

    style: 新增默认头像地址

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,8 @@
 const APP_BASE_URL = "http://192.168.0.188"
 const APP_URL_PORT = "8080"
+const DEFAULT_AVATAR = `/img/avatar.png`
 export{
     APP_BASE_URL,
-    APP_URL_PORT
+    APP_URL_PORT,
+    DEFAULT_AVATAR
 }
