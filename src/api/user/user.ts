29332f2c97214d74053e9daa7cec7629ce599710
lk <354378485@qq.com>
2024-02-02 22:05:20 +0800

    style: 获取已发布商品跟数量

--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -10,8 +10,15 @@ const requestUserInfo = ()=>request<User>("/user",{},"GET")
 // 用户首次登录完善个人信息
 const improveInfo = (data:UserPart)=>request("/improveInfo",data,"POST")
 
+// 获取用户已经发布的商品
+const releasedProducts  = ()=>request("/product/released",{},"GET")
+
+// 获取用户已发布商品的数量
+const releasedCount = ()=>request("/product/count",{},"GET")
 export {
     userInfoById,
     requestUserInfo,
-    improveInfo
+    improveInfo,
+    releasedProducts,
+    releasedCount
 }
