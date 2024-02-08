e42db660bb5eebf8b30d7dc970b749b1b4a027ab
lk <354378485@qq.com>
2024-02-08 18:15:03 +0800

    style: 售卖模式组件

--- src/components/goods/SellModeIcon.vue
+++ src/components/goods/SellModeIcon.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
+import { onMounted, reactive } from 'vue';
+import useProductStore from '@/stores/product/index'
+import { storeToRefs } from 'pinia';
+const productStore = useProductStore()
+const {sellModeList} = storeToRefs(productStore)
+const props = defineProps<{mode:number}>()
+const sellModeMap = reactive<string[]>([])
 
-type SellMode = "出售" | "出租" | "交换";
-const props = defineProps<{type:SellMode}>()
+onMounted(()=>{
+	sellModeMap.length = 0
+	sellModeList.value.forEach(item=>{
+		sellModeMap[item.id] = item.name
+	})
+})
 </script>
 <template>
     <view class="sell-mode-icon">
-        <text>{{ type }}</text>
+        <text>{{ sellModeMap[mode] }}</text>
     </view>
 </template>
 <style lang="scss" scoped>
