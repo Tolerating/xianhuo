e9258e24c32497a55262977ef876f027ea26e013
lk <354378485@qq.com>
2024-02-08 18:13:45 +0800

    feat: 价格显示组件

---
+++ src/components/goods/ProductPrice.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps<{ mode: number, currentPrice: string, originPrice: string, timeUnit: string }>()
+</script>
+<template>
+    <view class="goods-price">
+        <text>￥{{ mode == 1 ? currentPrice : currentPrice + "/" + timeUnit }}</text>
+        <text style="text-decoration: line-through;" v-if="parseFloat(originPrice) != 0">原价：￥{{ originPrice }}</text>
+    </view>
+</template>
+<style lang="scss" scoped>
+.goods-price {
+    margin-top: 2px;
+
+    text {
+        color: red;
+    }
+}
+</style>
