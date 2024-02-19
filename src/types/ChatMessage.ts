d58d4d7db30f6d87f6b393b351eb8d0b7a094f3b
lk <354378485@qq.com>
2024-02-19 22:33:50 +0800

    style: 聊天内容实体类

---
+++ src/types/ChatMessage.ts
@@ -0,0 +1,67 @@
+export interface ChatMessage {
+  /**
+   * 用于窗口滚动定位的id
+   *
+   * @type {number}
+   * @memberof ChatMessage
+   */
+  scrollId?: number,
+  /**
+   *聊天内容表主键
+   *
+   * @type {string}
+   * @memberof ChatMessage
+   */
+  messageId?:string,
+  /**
+   聊天关系表主键
+   *
+   *
+   * @type {string}
+   * @memberof ChatMessage
+   */
+  linkId: string,
+  /**
+   *发送内容
+   *
+   * @type {string}
+   * @memberof ChatMessage
+   */
+  content: string,
+  /**
+    接收方id
+   *
+   *
+   * @type {string}
+   * @memberof ChatMessage
+   */
+  toUser: string
+  /**
+   *发送方id
+   *
+   * @type {string}
+   * @memberof ChatMessage
+   */
+  fromUser: string,
+  /**
+   *发送时间
+   *
+   * @type {string}
+   * @memberof ChatMessage
+   */
+  sendTime: string,
+  /**
+   * 消息类型，1表示文本，2表示图片
+   *
+   * @type {number}
+   * @memberof ChatMessage
+   */
+  type?:number,
+  /**
+   *是否为最后一条消息，1表示是，0表示不是
+   *
+   * @type {number}
+   * @memberof ChatMessage
+   */
+  idLatest?:number
+}
